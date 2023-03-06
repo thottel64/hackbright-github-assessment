e07d0d585fe0e83245d80912cd12f94d0e8e6a00
thottel64 <105885349+thottel64@users.noreply.github.com>
2023-03-05 18:27:33 -0600

    added subtract function

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World!")
 	sum := add(2, 3)
-	fmt.Println("the sum is", sum)
+	fmt.Println("the sum is: ", sum)
+	diff := subtract(5, 2)
+	fmt.Println("the difference is: ", diff)
 }
 
 func add(x int, y int) int {
 	return x + y
+}
+
+func subtract(x int, y int) int {
+	return x - y
 }
